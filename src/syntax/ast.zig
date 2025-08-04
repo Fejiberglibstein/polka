bff6c4fb2ff515c89eb02d1f578365de398bf8ef
Austin Thein <amthein321@gmail.com>
2025-08-04 19:58:59 -0400

    Change how ast computes else body in a conditional

--- src/syntax/ast.zig
+++ src/syntax/ast.zig
@@ -398,12 +398,7 @@ pub const Conditional = struct {
     }
 
     pub fn elseBody(self: Conditional, all_nodes: []const SyntaxNode) ?TextNode {
-        var iter = ASTIterator(TextNode).init(self.v, all_nodes);
-
-        // Skip past the if body
-        iter.skip(1);
-
-        return iter.next();
+        return castLastChild(self.v, all_nodes, TextNode);
     }
 };
 
