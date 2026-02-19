200801ba2c4368094a0f925b4a02ed1c6ab7f2b2
Austin Thein <amthein321@gmail.com>
2026-02-18 23:27:04 -0500

    Add ast.toTyped, & ASTIterator to ast

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,12 @@
 pub fn main() !void {
     var lexer = Lexer.init("", .text, "#");
     _ = lexer.next();
+
+    const nodes = try parser.parse("", .text, std.heap.page_allocator);
+
+    const f = ast.toTyped(ast.Text, nodes[nodes.len - 1]) orelse unreachable;
+    var iter = f.parts(&.{});
+    _ = iter.next();
 }
 
 const std = @import("std");
@@ -8,3 +14,4 @@ const std = @import("std");
 const Lexer = @import("syntax/Lexer.zig");
 const SyntaxNode = @import("syntax/node.zig").SyntaxNode;
 const parser = @import("syntax/parser.zig");
+const ast = @import("syntax/ast.zig");
--- src/syntax/ast.zig
+++ src/syntax/ast.zig
@@ -49,155 +49,194 @@
 //!
 //! ## Implementation
 //!
-//! A node of the AST has a `toTyped` function that converts a SyntaxNode into itself, and a `kind`
-//! indicating what the kind of the node is. The kind is used to derive the toTyped function.
-//!
 //! Some derived nodes are not rooted in the CST, such as `Access`; there is a `dot_access` and
 //! `bracket_access` SyntaxKind but no `access` SyntaxKind. These derived nodes simplify the AST to
 //! not worry about the specific kind.
-//!
-//! A node of the AST has a `toTyped` function that converts a SyntaxNode into itself, and a `kind`
-//! indicating what the kind of the node is. The kind is used to derive the toTyped function.
 //!
 //! Some derived nodes are not rooted in the CST, such as `Access`; there is a `dot_access` and
 //! `bracket_access` SyntaxKind but no `access` SyntaxKind. These derived nodes simplify the AST to
 //! not worry about the specific kind.
 
-fn deriveToTyped(T: type) fn (SyntaxNode) ?T {
-    return struct {
-        fn toTyped(node: SyntaxNode) ?T {
-            return if (node.kind == T.kind)
-                T{ .v = node }
-            else
-                null;
+pub fn toTyped(T: type, node: SyntaxNode) ?T {
+    if (@typeInfo(T) == .@"struct") {
+        return if (node.kind == T.kind)
+            T{ .v = node }
+        else
+            null;
+    } else if (@typeInfo(T) == .@"union") {
+        // TODO check if this optimizes into a jump table
+        inline for (@typeInfo(T).@"union".fields) |field| {
+            if (toTyped(field.type, node)) |v| return @unionInit(T, field.name, v);
         }
-    }.toTyped;
+        return null;
+    }
 }
+
+/// Iterator to yield all the child nodes that coerce to an AST type
+fn ASTIterator(T: type) type {
+    return struct {
+        child_nodes: []const SyntaxNode,
+        index: usize,
+
+        pub fn init(node: SyntaxNode, all_nodes: []const SyntaxNode) @This() {
+            return .{ .child_nodes = node.getTreeChildren(all_nodes), .index = 0 };
+        }
+
+        pub fn next(self: *@This()) ?T {
+            while (self.index < self.child_nodes.len) : (self.index += 1) {
+                if (toTyped(T, self.child_nodes[self.index])) |child| {
+                    self.index += 1;
+                    return child;
+                }
+            }
+            return null;
+        }
+    };
+}
+
+/// Used in the typed AST to get children matching a certain ASTNode type.
+fn castLastChild(T: type, node: SyntaxNode, all_nodes: []const SyntaxNode) ?T {
+    const children = node.getTreeChildren(all_nodes);
+    if (children.len == 0) return null;
+
+    var i = children.len - 1;
+    while (i > 0) : (i -= 1) {
+        if (toTyped(T, children[i])) |c| return c;
+    }
+    return null;
+}
+
+/// Used in the typed AST to get children matching a certain ASTNode type.
+fn getFirstChild(T: type, node: SyntaxNode, all_nodes: []const SyntaxNode) ?T {
+    for (node.getTreeChildren(all_nodes)) |child| {
+        if (toTyped(T, child)) |c| return c;
+    }
+    return null;
+}
+
+pub const Text = struct {
+    v: SyntaxNode,
+    const kind: SyntaxKind = .text;
+
+    pub fn parts(self: Text, all_nodes: []const SyntaxNode) ASTIterator(TextPart) {
+        return .init(self.v, all_nodes);
+    }
+};
+
+pub const TextPart = union(enum) {
+    newline: Newline,
+    text: TextLine,
+    code: Code,
+};
+
+pub const TextLine = struct {
+    v: SyntaxNode,
+    const kind: SyntaxKind = .text_line;
+};
+
+pub const Newline = struct {
+    v: SyntaxNode,
+    const kind: SyntaxKind = .newline;
+};
 
 pub const List = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .list;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const Dict = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .dict;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const DictField = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .dict_field;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const Code = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .code;
-    pub const toTyped = deriveToTyped(@This());
-};
-
-pub const Text = struct {
-    v: SyntaxNode,
-    const kind: SyntaxKind = .text;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const Grouping = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .grouping;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const FunctionDef = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .function_def;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const FunctionParameters = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .function_parameters;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const ReturnStatement = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .return_statement;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const LetStatement = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .let_statement;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const ExportStatement = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .export_statement;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const ForLoop = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .for_loop;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const WhileLoop = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .while_loop;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const Conditional = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .conditional;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const Unary = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .unary;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const Binary = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .binary;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const FunctionCall = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .function_call;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const FunctionArgs = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .function_args;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const DotAccess = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .dot_access;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const BracketAccess = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .bracket_access;
-    pub const toTyped = deriveToTyped(@This());
 };
 
 pub const Number = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .number;
-    pub const toTyped = deriveToTyped(@This());
 
     pub fn get(self: Number, src: []const u8) f64 {
         return std.fmt.parseFloat(f64, self.v.getLeafSource(src)) catch unreachable;
@@ -207,7 +246,6 @@ pub const Number = struct {
 pub const String = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .string;
-    pub const toTyped = deriveToTyped(@This());
 
     pub fn get(self: String, src: []const u8) []const u8 {
         // TODO make this handle backslashes
@@ -219,34 +257,34 @@ pub const String = struct {
 pub const Ident = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .ident;
-    pub const toTyped = deriveToTyped(@This());
 
     pub fn get(self: Ident, src: []const u8) []const u8 {
         return self.v.getLeafSource(src);
     }
 };
 
+pub const Bool = union(enum) {
+    true: True,
+    false: False,
+
+    pub fn get(self: Bool) bool {
+        return self.v == Bool.true;
+    }
+};
+
+pub const True = struct {
+    v: SyntaxNode,
+    const kind: SyntaxKind = .keyword_true;
+};
+
+pub const False = struct {
+    v: SyntaxNode,
+    const kind: SyntaxKind = .keyword_false;
+};
+
 pub const Nil = struct {
     v: SyntaxNode,
     const kind: SyntaxKind = .keyword_nil;
-    pub const toTyped = deriveToTyped(@This());
-};
-
-pub const Bool = struct {
-    v: SyntaxNode,
-
-    pub fn toTyped(node: SyntaxNode) ?Bool {
-        return switch (node.kind) {
-            .keyword_true => .{ .v = node },
-            .keyword_false => .{ .v = node },
-
-            else => null,
-        };
-    }
-
-    pub fn get(self: Bool) bool {
-        return self.v.kind == .keyword_true;
-    }
 };
 
 const SyntaxKind = @import("node.zig").SyntaxKind;
