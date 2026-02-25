5ea5968a812662145c3494a12290291539e13e02
Austin Thein <amthein321@gmail.com>
2026-02-25 14:42:58 -0500

    Add errors and some node compilation to compiler

--- src/lang/Compiler.zig
+++ src/lang/Compiler.zig
@@ -11,7 +11,7 @@ gpa: std.mem.Allocator,
 
 current_chunk: Chunk,
 variables: std.ArrayList(Variable),
-errors: std.ArrayList(CompilationError),
+errors: std.ArrayList(CompilerErrorPayload),
 
 const Compiler = @This();
 
@@ -33,6 +33,12 @@ pub fn getVariable(self: *Compiler, var_name: []const u8) ?Variable {
     return null;
 }
 
+pub fn setError(self: *Compiler, node: SyntaxNode, kind: CompilerErrorPayload.Kind) !noreturn {
+    try self.errors.append(self.gpa, .{ .node = node, .kind = kind });
+
+    return CompilerError.Error;
+}
+
 pub fn compile(self: *Compiler) !Chunk {
     if (self.all_nodes.len == 0) return self.current_chunk;
     const root = ast.toASTNode(ast.Text, self.all_nodes[self.all_nodes.len - 1]) orelse unreachable;
@@ -45,12 +51,13 @@ fn compileText(self: *Compiler, text: ast.Text) !void {
     while (parts.next()) |part| {
         switch (part) {
             .text_line => |line| {
-                try self.current_chunk.addInstruction(self.gpa, .write, .{line.get(self.src)});
+                try self.current_chunk.appendInstruction(self.gpa, .write);
+                try self.current_chunk.appendBytes(self.gpa, std.mem.asBytes(&line.get(self.src)));
             },
             .newline => {
-                var newline: []const u8 = undefined;
-                newline = "\n";
-                try self.current_chunk.addInstruction(self.gpa, .write, .{newline});
+                try self.current_chunk.appendInstruction(self.gpa, .write);
+                const newline: []const u8 = "\n";
+                try self.current_chunk.appendBytes(self.gpa, std.mem.asBytes(&newline));
             },
             .code => |code| try self.compileCode(code),
         }
@@ -73,26 +80,72 @@ fn compileCode(self: *Compiler, code: ast.Code) !void {
     }
 }
 
-fn compileExpression(self: *Compiler, expr: ast.Expression) !void {
+fn compileExpression(self: *Compiler, expr: ast.Expression) CompilerError!void {
     switch (expr) {
-        .nil => {},
+        .nil => try self.current_chunk.appendInstruction(self.gpa, .push_nil),
         .list => {},
         .dict => {},
-        .true => {},
+        .true => try self.current_chunk.appendInstruction(self.gpa, .push_true),
         .unary => {},
         .ident => {},
-        .false => {},
-        .binary => {},
+        .false => try self.current_chunk.appendInstruction(self.gpa, .push_false),
+        .binary => |node| try self.compileBinary(node),
         .number => {},
         .string => {},
-        .grouping => {},
+        .integer => |node| {
+            const int: u32 = node.get(self.src) catch {
+                try self.setError(node.node, .number_too_large);
+            };
+
+            if (std.math.cast(u8, int)) |n| {
+                try self.current_chunk.appendInstruction(self.gpa, .push_u8);
+                try self.current_chunk.appendBytes(self.gpa, std.mem.asBytes(&n));
+            } else @panic("TODO");
+        },
+        .grouping => |node| try self.compileExpression(node.inner(self.all_nodes)),
         .conditional => {},
         .function_def => {},
         .function_call => {},
     }
 }
 
-const CompilationError = struct {};
+fn compileBinary(self: *Compiler, binary: ast.Binary) CompilerError!void {
+    try self.compileExpression(binary.lhs(self.all_nodes));
+    try self.compileExpression(binary.lhs(self.all_nodes));
+
+    const instruction: InstructionKind = switch (binary.op(self.all_nodes)) {
+        .in => .binary_in,
+        .@"or" => .binary_or,
+        .add => .binary_add,
+        .assign => @panic("TODO"),
+        .equal => .binary_equ,
+        .@"and" => .binary_and,
+        .less_than => .binary_lt,
+        .divide => .binary_div,
+        .multiply => .binary_mul,
+        .subtract => .binary_sub,
+        .modulo => .binary_mod,
+        .greater_than => .binary_gt,
+        .not_equal => .binary_neq,
+        .less_than_equal => .binary_lte,
+        .greater_than_equal => .binary_gte,
+    };
+
+    try self.current_chunk.appendInstruction(self.gpa, instruction);
+}
+
+const CompilerErrorPayload = struct {
+    /// The node that caused the error
+    node: SyntaxNode,
+    kind: Kind,
+
+    const Kind = union(enum) {
+        /// Integer literal is too large
+        number_too_large,
+    };
+};
+
+const CompilerError = error{Error} || std.mem.Allocator.Error;
 
 const std = @import("std");
 const InstructionKind = @import("bytecode.zig").InstructionKind;
