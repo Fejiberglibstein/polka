67ece56fe54217871edfd2979e4d64bd77b334d2
Austin Thein <amthein321@gmail.com>
2026-02-28 15:27:37 -0500

    Vm stack stuff

--- src/lang/eval/Vm.zig
+++ src/lang/eval/Vm.zig
@@ -1,39 +1,51 @@
 const Variable = struct {
     /// The name of the variable
     name: []const u8,
-    /// The index into the stack frame where this variable lives.
-    frame_index: u32,
+    stack_position: usize,
 };
 
 src: []const u8,
 all_nodes: []const SyntaxNode,
 gpa: std.mem.Allocator,
+output: std.Io.Writer,
 
-variables: std.ArrayList(Variable),
 errors: std.ArrayList(RuntimeErrorPayload),
+stack: std.ArrayList(Value),
+variables: std.ArrayList(Variable),
 
 const Vm = @This();
+const stack_size = 1024;
 
-pub fn init(all_nodes: []const SyntaxNode, src: []const u8, gpa: std.mem.Allocator) Vm {
+pub fn init(
+    all_nodes: []const SyntaxNode,
+    src: []const u8,
+    gpa: std.mem.Allocator,
+    output: std.Io.Writer,
+) !Vm {
     return .{
-        .all_nodes = all_nodes,
-        .errors = .empty,
         .gpa = gpa,
         .src = src,
-        .variables = .empty,
+        .errors = .empty,
+        .output = output,
+        .all_nodes = all_nodes,
+        .stack = try .initCapacity(gpa, stack_size),
+        .variables = try .initCapacity(gpa, stack_size),
     };
-}
-
-pub fn getVariable(self: *Vm, var_name: []const u8) ?Variable {
-    for (self.variables.items) |variable| {
-        if (variable.name == var_name) return variable;
-    }
-    return null;
 }
 
 pub fn setError(self: *Vm, node: SyntaxNode, kind: RuntimeErrorPayload.Kind) !noreturn {
     try self.errors.append(self.gpa, .{ .node = node, .kind = kind });
     return RuntimeError.Error;
+}
+
+pub fn stackPush(self: *Vm, ctx: SyntaxNode, v: Value) !void {
+    self.stack.appendBounded(v) catch try self.setError(ctx, .stack_overflow);
+}
+pub fn stackPop(self: *Vm) Value {
+    return self.stack.pop() catch unreachable;
+}
+pub fn stackPeek(self: *Vm, back: usize) Value {
+    return self.stack[self.stack.items.len - back - 1];
 }
 
 const RuntimeErrorPayload = struct {
@@ -43,13 +55,15 @@ const RuntimeErrorPayload = struct {
     const Kind = union(enum) {
         /// Integer literal is too large
         number_too_large,
+        stack_overflow,
     };
 };
 
-const RuntimeError = error{
+pub const RuntimeError = error{
     Error,
 } || std.mem.Allocator.Error;
 
 const std = @import("std");
-const SyntaxNode = @import("syntax//node.zig").SyntaxNode;
-const ast = @import("syntax/ast.zig");
+const SyntaxNode = @import("../syntax/node.zig").SyntaxNode;
+const ast = @import("../syntax/ast.zig");
+const Value = @import("value.zig").Value;
