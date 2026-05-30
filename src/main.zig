23b9f650794385e1b42412e25d542e170054b14f
Austin Thein <amthein321@gmail.com>
2026-05-30 16:38:18 -0400

    Create basic main program

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,35 @@
-pub fn main() !void {
-    const gpa = std.heap.smp_allocator;
-    var threaded = std.Io.Threaded.init(gpa, .{});
-    const io = threaded.io();
+pub fn main(init: std.process.Init) !void {
+    const gpa = init.gpa;
+    const io = init.io;
+    const environ_map = init.environ_map;
+
+    var stdout_buf: [2048]u8 = undefined;
+    var stdout_writer = std.Io.File.stdout().writer(io, &stdout_buf);
+    const stdout = &stdout_writer.interface;
+    defer stdout.flush() catch {};
+
+    var stderr_buf: [2048]u8 = undefined;
+    var stderr_writer = std.Io.File.stderr().writer(io, &stderr_buf);
+    const stderr = &stderr_writer.interface;
+    defer stderr.flush() catch {};
 
     var pool: Value.String.Pool = .init(gpa);
     defer pool.deinit();
 
-    var config: polka.Config = .init(gpa, &pool);
-    defer config.deinit();
+    var runner: Runner = try .init(io, gpa, environ_map, &pool);
+    defer runner.deinit(io);
 
-    const constants = try builtin.Constants.init(io, gpa, &pool);
-    defer constants.deinit(gpa);
+    try runner.sync(io);
+    try stdout.writeAll(runner.error_log.written());
 
-    var value_allocator = std.heap.ArenaAllocator.init((std.heap.page_allocator));
-    defer value_allocator.deinit();
-
-    const parsed = try parser.parse("", .text, gpa);
-    defer parsed.deinit(std.heap.page_allocator);
-
-    var output: std.Io.Writer.Allocating = .init(gpa);
-    defer output.deinit();
-    const writer = &output.writer;
-
-    var vm = try Vm.init(gpa, .{
-        .src = "",
-        .output = writer,
-        .config = &config,
-        .string_pool = &pool,
-        .nodes = parsed.nodes,
-        .constants = constants,
-        .value_allocator = value_allocator.allocator(),
-    });
-    if (vm.run()) |err| {
-        std.debug.print("Error: {f}", .{err.formatWith(parsed.nodes, "", &pool)});
-    }
-
-    _ = builtin.functions.get("hfk");
+    try runner.vfs.print(stdout);
 }
 
 test {
     _ = @import("lang/test_eval.zig");
     _ = @import("lang/test_syntax.zig");
+
+    std.testing.refAllDecls(@import("VirtualFilesystem.zig"));
 }
 
 const std = @import("std");
@@ -51,3 +40,4 @@ const Vm = @import("lang.zig").Vm;
 const Value = @import("lang.zig").Value;
 const builtin = @import("lang.zig").builtin;
 const polka = @import("polka.zig");
+const Runner = @import("Runner.zig");
